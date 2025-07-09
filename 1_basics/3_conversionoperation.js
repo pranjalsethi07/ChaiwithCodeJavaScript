4a1fdb17c09d08ff1b270e99eeea9e67f64daf11
pranjalsethi07 <163377712+pranjalsethi07@users.noreply.github.com>
2025-07-09 14:40:06 +0000

    conversion of datatypes

---
+++ 1_basics/3_conversionoperation.js
@@ -0,0 +1,56 @@
+// during datatype conversion,datatye is used in Uppercase
+// let score="33"
+// console.log(typeof(score));
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); //NaN
+
+// let score=null
+// console.log(typeof(score));
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); //NaN
+
+let score=undefined
+console.log(typeof(score));
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber); //NaN
+
+
+// SUMMARY
+// "33"-> 33
+// "33abc">> NaN
+// true->1, false-0
+// Undefined --> NaN
+// null >>  0
+ 
+// let isLoggedIn= "1"
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)  outut is TRUE
+
+// let isLoggedIn= "pranjal"
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)   OUTPUT IS TRUE
+
+
+// let isLoggedIn= ""
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)  
+
+// let isLoggedIn= " "
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)  
+
+let marks=33
+let stringMarks= String(marks)
+console.log(marks)
+console.log(typeof marks)
+console.log(typeof stringMarks)
+
+
+
+
