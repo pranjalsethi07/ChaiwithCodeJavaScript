4f55d7322a8b1929e62a312e33fb3f1551248eb6
pranjalsethi07 <163377712+pranjalsethi07@users.noreply.github.com>
2025-07-09 07:22:19 +0000

    variablesnddatatypes

---
+++ 1_basics/1variables.js
@@ -0,0 +1,12 @@
+const accountId=1234566
+let accountEmail="pranjal @google.com"
+var accountPassword="78498239"
+accountCity="Jaipur"
+// accountId=2
+console.log(accountId);
+accountEmail="fjnkskjns@gmail.com"
+accountPassword="324421"
+accountCity="bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/* becoz of issue in block scope and funcn scope prefer not using var */
---
+++ 1_basics/2datatyes.js
@@ -0,0 +1,28 @@
+// "use strict"; trat all code as new version of js 
+// alert(3+3) ; doesn't work...we are running in nodejs not in browser
+// tc39 and mdn documentation
+
+
+/*null is a standalone tye and a data tye, it is a rer of emty value and can be ut null,
+null is an object
+number-2^53
+big Int
+String
+boolean
+undefined
+symbol=>unique
+
+abv are primitive datatypes */
+
+// typeof-tells datatype
+
+
+// typeof null is object and typeof undefined is undefined 
+
+const accountId=1234566
+let accountEmail="pranjal @google.com"
+var accountPassword="78498239"
+accountCity="Jaipur"
+console.log(typeof accountId)
+console.log(typeof accountPassword)
+
